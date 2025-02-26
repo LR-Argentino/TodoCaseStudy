d6da8897cc43a602458b1b149f3ceb9780390d45
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-02-26 18:39:21 +0100

    Initial Commit

---
+++ TodoCaseStudy/TodoCaseStudy.h
@@ -0,0 +1,18 @@
+//
+//  TodoCaseStudy.h
+//  TodoCaseStudy
+//
+//  Created by Luca Argentino on 26.02.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TodoCaseStudy.
+FOUNDATION_EXPORT double TodoCaseStudyVersionNumber;
+
+//! Project version string for TodoCaseStudy.
+FOUNDATION_EXPORT const unsigned char TodoCaseStudyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TodoCaseStudy/PublicHeader.h>
+
+
