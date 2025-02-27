09b79b9fcc412ce7dadff3f8ece2af788c8f0c42
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-02-27 11:55:18 +0100

    setup iOS Target

---
+++ TodoCaseStudyIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoCaseStudyIOS
+//
+//  Created by Luca Argentino on 27.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
